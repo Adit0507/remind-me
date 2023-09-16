3fff0f60c3b0d43ac4464b37445ad527bc52873a
Adit0507 <adi05072002@gmail.com>
2023-09-16 16:23:04 +0530

    implementing forms

--- components/CreateCollectionSheet.tsx
+++ components/CreateCollectionSheet.tsx
@@ -7,12 +7,72 @@ import {
   SheetTitle,
 } from "./ui/sheet";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormLabel,
+  FormItem,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "./ui/form";
+
+import { toast } from "./ui/use-toast";
+import { Input } from "./ui/input";
+
+import { CollectionColors, CollectionColor } from "@/lib/constants";
+
+import {
+  createCollectionSchema,
+  createCollectionSchemaType,
+} from "@/schema/createCollection";
+
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { createCollection } from "@/actions/collection";
+
 interface Props {
   open: boolean;
   onOpenChange: (open: boolean) => void;
 }
 
 const CreateCollectionSheet = ({ open, onOpenChange }: Props) => {
+  const form = useForm<createCollectionSchemaType>({
+    defaultValues: {},
+    resolver: zodResolver(createCollectionSchema),
+  });
+
+  const router = useRouter();
+
+  const onSubmit = async (data: createCollectionSchemaType) => {
+    try {
+      await createCollection(data);
+      // close sheet
+      openChangeWrapper(false);
+      router.refresh();
+
+      toast({
+        title: "SUCCESS",
+        description: "collection created successfully",
+      });
+    } catch (error: any) {
+      toast({
+        title: "ERROR",
+        description: "Something went wrong. Plz try again",
+        variant: "destructive",
+      });
+
+      console.log("Error while creating collection", error);
+    }
+  };
+
+  const openChangeWrapper = (open: boolean) => {
+    form.reset();
+    onOpenChange(open);
+  };
+
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
       <SheetContent>
@@ -22,6 +82,31 @@ const CreateCollectionSheet = ({ open, onOpenChange }: Props) => {
             Collections are a way to group your tasks
           </SheetDescription>
         </SheetHeader>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 flex flex-col"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Personal" {...field} />
+                  </FormControl>
+                  <FormDescription>Collection Name</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            
+
+          </form>
+        </Form>
       </SheetContent>
     </Sheet>
   );
