23783c0248e0c9ef1d55110fd4f7625bd57bb8b7
Adit0507 <adi05072002@gmail.com>
2023-09-24 11:48:06 +0530

    small change

--- components/CreateCollectionBtn.tsx
+++ components/CreateCollectionBtn.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 import CreateCollectionSheet from "./CreateCollectionSheet";
 
 const CreateCollectionBtn = () => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const handleOpenChange = (open: boolean) => setOpen(open);
 
   return (
